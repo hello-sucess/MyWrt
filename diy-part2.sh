43b4e32e0861cdad68622fa577ced90547e5c3b2
Coffee <31380144+hello-sucess@users.noreply.github.com>
2023-05-10 10:36:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
-sed -i 's/luci-theme-bootstrap/luci-app-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
